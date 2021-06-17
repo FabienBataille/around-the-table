d684be22448e22546ae1a14c7884554d150c9e50
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-06-17 13:38:58 +0200

    header + footer mobile

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-relative">
+      <div className="Footer">
+        <ul>
+          <li>Accueil</li>
+          <li>Connection</li>
+          <li>Profil</li>
+          <li>Nous rejoindre</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
