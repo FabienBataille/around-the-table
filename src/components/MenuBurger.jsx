b643fcdcceacb40bf69ade70b8ec3ab36527fd51
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-06-17 10:09:31 +0200

    header mobile

--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -1,20 +1,28 @@
 import './menuBurger.css';
-import burger from '../images/burger.png'
+import burger from '../images/burger.png';
 
 function MenuBurger() {
   return (
-    <div className="MenuBurger">
+    <div className='MenuBurger'>
       <ul className='list'>
-      <li>
-        <img className="logoBurger" alt="logoBurger" src={burger}/>
-        <ul>
-          <li><a href="#">Javascript</a></li>
-          <li><a href="#">React</a></li>
-          <li><a href="#">Node</a></li>
-          <li><a href="#">SQL</a></li>
-        </ul>
-      </li>
-    </ul>
+        <li>
+          <img className='logoBurger' alt='logoBurger' src={burger} />
+          <ul>
+            <li>
+              <a href='#'>Javascript</a>
+            </li>
+            <li>
+              <a href='#'>React</a>
+            </li>
+            <li>
+              <a href='#'>Node</a>
+            </li>
+            <li>
+              <a href='#'>SQL</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 }
