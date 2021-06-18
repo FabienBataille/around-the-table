4457f5fccfe423726eab06a1960c22550610b5dc
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-06-18 15:32:09 +0200

    mise en page profil

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Profil from './components/Profil';
+import Profil from './components/Profile';
 
 function App() {
   return (
--- src/components/Profil.jsx
+++
@@ -1,9 +0,0 @@
-function Profil() {
-  return (
-    <div className="Profil">
-      <h1 className="titre">Profil</h1>
-    </div>
-  );
-}
-
-export default Profil;
