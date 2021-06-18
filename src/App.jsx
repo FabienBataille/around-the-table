b036a08c733794ff0fdfb2347956e9c72147895e
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-06-18 11:54:32 +0200

    mise en page nous rejoindre

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Accueil from './components/Accueil';
+import NousRejoindre from './components/NousRejoindre';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Accueil />
+      <NousRejoindre />
       <Footer />
     </div>
   );
